453ceca1f731cff626d8a0d3e86fb0d5ed85716b
Pitusd-V2 <118129946+Pitusd-V2@users.noreply.github.com>
2023-09-18 18:35:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function a() {
+    open("aboutme.html")
+}
+
+function main() {
+    a();
+}
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function() {
+  loader.style.display = "none";
+})
+
+function b() {
+  open("https://t.me/Danieleppz")
+}
+
+function main2() {
+  b();
+}
